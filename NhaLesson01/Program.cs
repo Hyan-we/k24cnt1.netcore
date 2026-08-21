5fbdfbe4f8353b0f0b66a93f76fc122e25723af8
Nguyen Huy Anh <saturngamingtv@gmail.com>
2026-08-21 14:49:10 +0700

    NhaLesson01 - First WebApp

---
+++ NhaLesson01/Program.cs
@@ -0,0 +1,15 @@
+namespace NhaLesson01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello Huy Anh!");
+
+            app.Run();
+        }
+    }
+}
