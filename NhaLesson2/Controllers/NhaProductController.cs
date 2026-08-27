145c108dfa50d3718b43da0ba05d429dbab2b11a
Nguyen Huy Anh <saturngamingtv@gmail.com>
2026-08-27 11:57:49 +0700

    NhaLesson2

---
+++ NhaLesson2/Controllers/NhaProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using NhaLesson2.Models;
+
+namespace NhaLesson2.Controllers
+{
+    public class NhaProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            //dua du lieu ra view
+
+            ViewBag.name = "huy anh";
+            ViewData["address"] = "fit NTU";
+            TempData["UNI"] = "Truong Dai Hoc Nguyen Trai";
+            return View();
+        }
+
+        //chi tiet san pham
+        public IActionResult GetProduct()
+        {
+            //Mock Data
+            NhaProduct nhaProduct = new NhaProduct()
+            {
+                ProductId = "P001",
+                ProductName = "Iphone 14 Pro Max",
+                YearRelease = 2022,
+                Price = 11000000,
+            };
+            ViewData["productVD"] = nhaProduct;
+            ViewBag.productVB = nhaProduct;
+            return View(nhaProduct);
+        }
+    }
+}
---
+++ NhaLesson2/Models/NhaProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace NhaLesson2.Models
+{
+    public class NhaProduct
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
