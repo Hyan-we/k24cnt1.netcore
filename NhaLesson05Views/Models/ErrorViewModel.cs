ea718cb232db7ab44cbf5b72278dd7c38820e505
Nguyen Huy Anh <saturngamingtv@gmail.com>
2026-09-06 21:14:50 +0700

    NhaLesson05Views

---
+++ NhaLesson05Views/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhaLesson05Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
