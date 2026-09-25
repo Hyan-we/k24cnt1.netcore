a5458d65aaa1e6b0d34f2bc6ea4b1983481c1e71
Nguyen Huy Anh <saturngamingtv@gmail.com>
2026-09-25 21:19:27 +0700

    NhaLesson09

---
+++ NhaLesson09/Models/DataModels/NhaMember.cs
@@ -0,0 +1,13 @@
+﻿namespace NhaLesson09.Models.DataModels
+{
+    public class NhaMember
+    {
+        public int NhaMemberId { get; set; }
+        public string NhaMemberUserName { get; set; }
+        public string NhaMemberPassword { get; set; }
+        public string NhaMemberEmail { get; set; }
+        public string NhaMemberPhone { get; set; }
+        public string NhaMemberFullName { get; set; }
+        public DateTime NhaMemberBirthday { get; set; }
+    }
+}
